e1907bd974bcf6d70b8a74f10b24f0357e8f764f
Titus Wormer <tituswormer@gmail.com>
2022-11-21 13:07:22 +0100

    Add improved types

--- build.js
+++ build.js
@@ -2,18 +2,24 @@ import fs from 'node:fs'
 import path from 'node:path'
 import chalk from 'chalk'
 
+const prefix = `// @ts-expect-error: this registers types for the language files.
+/** @typedef {import('highlight.js/types/index.js')} DoNotTochItRegistersLanguageFiles */
+`
+
 fs.writeFileSync(
   path.join('lib', 'all.js'),
-  String(
-    fs.readFileSync(path.join('node_modules', 'lowlight', 'lib', 'all.js'))
-  ).replace(/\blowlight\b/g, 'emphasize')
+  prefix +
+    String(
+      fs.readFileSync(path.join('node_modules', 'lowlight', 'lib', 'all.js'))
+    ).replace(/\blowlight\b/g, 'emphasize')
 )
 
 fs.writeFileSync(
   path.join('lib', 'common.js'),
-  String(
-    fs.readFileSync(path.join('node_modules', 'lowlight', 'lib', 'common.js'))
-  ).replace(/\blowlight\b/g, 'emphasize')
+  prefix +
+    String(
+      fs.readFileSync(path.join('node_modules', 'lowlight', 'lib', 'common.js'))
+    ).replace(/\blowlight\b/g, 'emphasize')
 )
 
 console.log(chalk.green('✓') + ' wrote `all`, `common`')

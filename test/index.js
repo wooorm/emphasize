3fdf9822d2da4e6f3b6f8bfa6d3d1b411a117496
Titus Wormer <tituswormer@gmail.com>
2022-11-21 13:16:36 +0100

    Use Node test runner

--- test/index.js
+++ test/index.js
@@ -1,16 +1,17 @@
+import assert from 'node:assert/strict'
 import fs from 'node:fs/promises'
 import process from 'node:process'
-import test from 'tape'
+import test from 'node:test'
 import chalk from 'chalk'
 import {isHidden} from 'is-hidden'
 import {emphasize} from '../lib/all.js'
 
 /* eslint-disable no-await-in-loop */
 
-test('emphasize.highlight(language, value[, sheet])', (t) => {
+test('emphasize.highlight(language, value[, sheet])', async (t) => {
   const result = emphasize.highlight('js', '')
 
-  t.throws(
+  assert.throws(
     () => {
       // @ts-expect-error runtime.
       emphasize.highlight(true)
@@ -19,7 +20,7 @@ test('emphasize.highlight(language, value[, sheet])', (t) => {
     'should throw when not given `string` for `name`'
   )
 
-  t.throws(
+  assert.throws(
     () => {
       // @ts-expect-error runtime.
       emphasize.highlight('js', true)
@@ -28,7 +29,7 @@ test('emphasize.highlight(language, value[, sheet])', (t) => {
     'should throw when not given `string` for `value`'
   )
 
-  t.throws(
+  assert.throws(
     () => {
       emphasize.highlight('fooscript', '')
     },
@@ -36,70 +37,68 @@ test('emphasize.highlight(language, value[, sheet])', (t) => {
     'should throw when given an unknown `language`'
   )
 
-  t.equal(result.relevance, 0, 'should return a `0` for `relevance` when empty')
+  assert.equal(
+    result.relevance,
+    0,
+    'should return a `0` for `relevance` when empty'
+  )
 
-  t.deepEqual(
+  assert.deepEqual(
     result.value,
     '',
     'should return an empty string for `value` when empty'
   )
 
-  t.deepEqual(
+  assert.deepEqual(
     emphasize.highlight('js', '# foo').value,
     '# foo',
     'should silently ignore illegals #1'
   )
 
-  t.test('fixture', (t) => {
+  await t.test('fixture', () => {
     const result = emphasize.highlight(
       'java',
       'public void moveTo(int x, int y, int z);'
     )
 
-    t.equal(
+    assert.equal(
       result.relevance,
       6,
       'should return the correct relevance for the fixture'
     )
 
-    t.equal(
+    assert.equal(
       result.language,
       'java',
       'should return the correct language for the fixture'
     )
 
-    t.deepEqual(
+    assert.deepEqual(
       result.value,
       '\u001B[32mpublic\u001B[39m \u001B[32mvoid\u001B[39m \u001B[34mmoveTo\u001B[39m(\u001B[33mint\u001B[39m x, \u001B[33mint\u001B[39m y, \u001B[33mint\u001B[39m z);',
       'should return the correct sequences for the fixture'
     )
-
-    t.end()
   })
 
-  t.test('custom `sheet`', (t) => {
+  await t.test('custom `sheet`', () => {
     const result = emphasize.highlight(
       'java',
       'public void moveTo(int x, int y, int z);',
       {keyword: chalk.bold, title: chalk.italic}
     )
 
-    t.deepEqual(
+    assert.deepEqual(
       result.value,
       '\u001B[1mpublic\u001B[22m \u001B[1mvoid\u001B[22m \u001B[3mmoveTo\u001B[23m(int x, int y, int z);',
       'should support custom sheets'
     )
-
-    t.end()
   })
-
-  t.end()
 })
 
-test('emphasize.highlightAuto(value[, settings | sheet])', (t) => {
+test('emphasize.highlightAuto(value[, settings | sheet])', async (t) => {
   const result = emphasize.highlightAuto('')
 
-  t.throws(
+  assert.throws(
     () => {
       // @ts-expect-error runtime.
       emphasize.highlightAuto(true)
@@ -108,80 +107,80 @@ test('emphasize.highlightAuto(value[, settings | sheet])', (t) => {
     'should throw when not given a string'
   )
 
-  t.equal(result.relevance, 0, 'should return a `0` for `relevance` when empty')
+  assert.equal(
+    result.relevance,
+    0,
+    'should return a `0` for `relevance` when empty'
+  )
 
-  t.equal(
+  assert.equal(
     result.language,
     null,
     'should return `null` for `language` when empty'
   )
 
-  t.deepEqual(
+  assert.deepEqual(
     result.value,
     '',
     'should return an empty string for `value` when empty'
   )
 
-  t.test('fixture', (t) => {
+  await t.test('fixture', () => {
     const result = emphasize.highlightAuto('"use strict";')
 
-    t.equal(
+    assert.equal(
       result.relevance,
       10,
       'should return the correct relevance for the fixture'
     )
 
-    t.equal(
+    assert.equal(
       result.language,
       'javascript',
       'should return the correct language for the fixture'
     )
 
-    t.deepEqual(
+    assert.deepEqual(
       result.value,
       '\u001B[35m"use strict"\u001B[39m;',
       'should return the correct sequences for the fixture'
     )
-
-    t.end()
   })
 
-  t.test('custom `sheet`', (t) => {
+  await t.test('custom `sheet`', () => {
     const result = emphasize.highlightAuto('"use strict";', {meta: chalk.bold})
 
-    t.deepEqual(
+    assert.deepEqual(
       result.value,
       '\u001B[1m"use strict"\u001B[22m;',
       'should support custom sheets'
     )
-
-    t.end()
   })
 
-  t.test('custom `subset`', (t) => {
+  await t.test('custom `subset`', () => {
     let result = emphasize.highlightAuto('"use strict";', {subset: ['java']})
 
-    t.equal(result.language, 'java', 'should support a given custom `subset`')
+    assert.equal(
+      result.language,
+      'java',
+      'should support a given custom `subset`'
+    )
 
-    t.doesNotThrow(() => {
+    assert.doesNotThrow(() => {
       result = emphasize.highlightAuto('"use strict";', {
         subset: ['fooscript', 'javascript']
       })
     }, 'should ignore unregistered subset languages (#1)')
 
-    t.equal(
+    assert.equal(
       result.language,
       'javascript',
       'should ignore unregistered subset languages (#2)'
     )
-
-    t.end()
   })
-
-  t.end()
 })
 
-test('fixtures', async (t) => {
+test('fixtures', async () => {
   const base = new URL('fixture/', import.meta.url)
   const files = await fs.readdir(base)
   let index = -1
@@ -211,14 +210,12 @@ test('fixtures', async (t) => {
       await fs.writeFile(new URL('output.txt', folder), actual + '\n')
     }
 
-    t.deepEqual(
+    assert.equal(
       actual,
       expected,
       'should correctly process ' + name + ' in ' + language
     )
   }
-
-  t.end()
 })
 
 /* eslint-enable no-await-in-loop */

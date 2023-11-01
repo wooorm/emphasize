74e3a84d1d4bd720c8d84d6f74e1942a1cd5bfa4
Titus Wormer <tituswormer@gmail.com>
2023-11-01 16:20:57 +0100

    Refactor to move files around

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /**
  * @typedef {import('lowlight').LanguageFn} LanguageFn
- * @typedef {import('./lib/core.js').AutoOptions} AutoOptions
- * @typedef {import('./lib/core.js').Sheet} Sheet
+ * @typedef {import('./lib/index.js').AutoOptions} AutoOptions
+ * @typedef {import('./lib/index.js').Sheet} Sheet
  */
 
 export {all, common} from 'lowlight'
-export {createEmphasize} from './lib/core.js'
+export {createEmphasize} from './lib/index.js'
